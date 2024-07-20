f58cf2050707fd9a7d7bb80d840a0b0a15eca8ee
Richard Beauchamp <rbeauchamp@users.noreply.github.com>
2024-07-20 07:18:36 -0700

    sp

---
+++ tests/RxDBDotNet.Tests/UserMutationTests.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net;
+
+namespace RxDBDotNet.Tests
+{
+    public class UserMutationTests
+    {
+        [Fact]
+        public async Task TestMethod1()
+        {
+            // Arrange
+            var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Example_AppHost>();
+            await using var app = await appHost.BuildAsync();
+            await app.StartAsync();
+
+            // Act
+            using var httpClient = app.CreateHttpClient("graphqlapi");
+            using var response = await httpClient.GetAsync(new Uri("/graphql", UriKind.Relative));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}

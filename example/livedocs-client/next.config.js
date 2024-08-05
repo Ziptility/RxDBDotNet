0edcf1f8c4e3895ff5b2dc2a5aef60e29e6f252b
Richard Beauchamp <rbeauchamp@users.noreply.github.com>
2024-08-05 14:06:02 -0700

    sp

--- example/livedocs-client/next.config.js
+++ example/livedocs-client/next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { dev, isServer }) => {
+    // Existing configurations
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
       bufferutil: 'commonjs bufferutil',
     });
+
+    // Enable source maps in development mode
+    if (dev && !isServer) {
+      config.devtool = 'eval-source-map';
+    }
+
     return config;
   },
   eslint: {

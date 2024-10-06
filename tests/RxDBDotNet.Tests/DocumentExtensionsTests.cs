3fd7c1a3e85d487e1e3368859683f7c492c9a28c
Richard Beauchamp <rbeauchamp@users.noreply.github.com>
2024-10-05 22:47:41 -0700

    increase code coverage

---
+++ tests/RxDBDotNet.Tests/DocumentExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using HotChocolate;
+using RxDBDotNet.Documents;
+
+namespace RxDBDotNet.Tests
+{
+    public class DocumentExtensionsTests
+    {
+        [Fact]
+        public void GetGraphQLTypeName_WithGraphQLNameAttribute_ShouldReturnAttributeName()
+        {
+            // Arrange
+            // Act
+            var result = DocumentExtensions.GetGraphQLTypeName<DocumentWithAttribute>();
+
+            // Assert
+            result.Should().Be("CustomTypeName");
+        }
+
+        [Fact]
+        public void GetGraphQLTypeName_WithoutGraphQLNameAttribute_ShouldReturnClassName()
+        {
+            // Arrange
+            // Act
+            var result = DocumentExtensions.GetGraphQLTypeName<DocumentWithoutAttribute>();
+
+            // Assert
+            result.Should().Be("DocumentWithoutAttribute");
+        }
+
+        [GraphQLName("CustomTypeName")]
+        private class DocumentWithAttribute : IReplicatedDocument
+        {
+            public Guid Id { get; set; }
+            public DateTimeOffset UpdatedAt { get; set; }
+            public bool IsDeleted { get; set; }
+            public List<string>? Topics { get; set; }
+        }
+
+        private class DocumentWithoutAttribute : IReplicatedDocument
+        {
+            public Guid Id { get; set; }
+            public DateTimeOffset UpdatedAt { get; set; }
+            public bool IsDeleted { get; set; }
+            public List<string>? Topics { get; set; }
+        }
+    }
+}
